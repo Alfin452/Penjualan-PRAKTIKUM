8d42d978e6a1849e4b25607f7e3adba0fb9c4542
Alfin452 <151822668+Alfin452@users.noreply.github.com>
2024-11-03 22:26:36 +0800

    Barang

---
+++ application/controllers/Barang.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Barang extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Barang_model');
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $data = array(
+            'title' => 'Dashboard',
+            'userlog' => InfoLogin(),
+            'barang' => $this->Barang_model->getAll(),
+            'content' => 'barang/index'
+        );
+        $this->load->view('template/main', $data);
+    }
+    public function add()
+    {
+        $data = array(
+            'title' => 'Tambah Data Barang',
+            'kategori' => $this->db->get('kategori')->result_array(),
+            'satuan' => $this->db->get('satuan')->result_array(),
+            'supplier' => $this->db->get('supplier')->result_array(),
+            'content' => 'barang/add_form'
+        );
+        $this->load->view('template/main', $data);
+    }
+    public function save()
+    {
+        $this->Barang_model->save();
+        if ($this->db->affected_rows() > 0) {
+            Sthis->session->set_flashdata("success", "Data Barang Berhasil DiSimpan");
+        }
+        redirect('barang');
+    }
+    public function getedit($id)
+    {
+        $data = array(
+            'title' => 'Update Data Barang',
+            'kategori' => $this->db->get('kategori')->result_array(),
+            'satuan' => $this->db->get('satuan')->result_array(),
+            'supplier' => $this->db->get('supplier')->result_array(),
+            'barang' => $this->Barang_model->getById($id),
+            'content' => 'barang/edit_form'
+        );
+        $this->load->view('template/main', $data);
+    }
+}
