b61815336599ca2ee154c699421c0cc73b2e61ca
Alfin452 <151822668+Alfin452@users.noreply.github.com>
2024-11-04 15:55:45 +0800

    Perbaikan ADD Barang

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -2,53 +2,60 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Barang extends CI_Controller
 {
-
     public function __construct()
     {
         parent::__construct();
         $this->load->model('Barang_model');
         $this->load->library('form_validation');
     }
-
     public function index()
     {
         $data = array(
             'title' => 'Dashboard',
-            'userlog' => InfoLogin(),
+            'userlog' => infoLogin(),
             'barang' => $this->Barang_model->getAll(),
             'content' => 'barang/index'
         );
         $this->load->view('template/main', $data);
     }
+
     public function add()
     {
         $data = array(
             'title' => 'Tambah Data Barang',
-            'kategori' => $this->db->get('kategori')->result_array(),
-            'satuan' => $this->db->get('satuan')->result_array(),
-            'supplier' => $this->db->get('supplier')->result_array(),
+            'kategori' => $this->db->get('Kategori')->result_array(),
+            'satuan' => $this->db->get('Satuan')->result_array(),
+            'supplier' => $this->db->get('Supplier')->result_array(),
             'content' => 'barang/add_form'
         );
         $this->load->view('template/main', $data);
     }
+
     public function save()
     {
         $this->Barang_model->save();
         if ($this->db->affected_rows() > 0) {
-            Sthis->session->set_flashdata("success", "Data Barang Berhasil DiSimpan");
+            $this->session->set_flashdata("success", "Data Barang Berhasil Disimpan");
         }
         redirect('barang');
     }
+
     public function getedit($id)
     {
         $data = array(
             'title' => 'Update Data Barang',
-            'kategori' => $this->db->get('kategori')->result_array(),
-            'satuan' => $this->db->get('satuan')->result_array(),
-            'supplier' => $this->db->get('supplier')->result_array(),
+            'kategori' => $this->db->get('Kategori')->result_array(),
+            'satuan' => $this->db->get('Satuan')->result_array(),
+            'supplier' => $this->db->get('Supplier')->result_array(),
             'barang' => $this->Barang_model->getById($id),
             'content' => 'barang/edit_form'
         );
         $this->load->view('template/main', $data);
+    }
+
+    public function delete($id)
+    {
+        $this->Barang_model->delete($id);
+        redirect('barang');
     }
 }
--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -3,11 +3,11 @@
         <h1 class="mt-4"></h1>
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item"><a href="<?php echo site_url('barang') ?>">Barang</a></li>
-            <li class="breadcrumb-item active"><?php echo $title ?></li>
+            <li class="breadcrumb-item active"><?php echo $title; ?></li>
         </ol>
         <div class="card mb-4">
             <div class="card-header">
-                <a href="<?php echo site_url('barang/add') ?>"><i class="fas fa-plus"></i> Add New</a>
+                <a href="<?php echo site_url('barang/add'); ?>"><i class="fas fa-plus"></i> Add New</a>
             </div>
             <?php if ($this->session->flashdata('success')): ?>
                 <div class="alert alert-success" role="alert">
@@ -16,11 +16,11 @@
             <?php endif; ?>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class="table table-striped table-bordered table-hover" width="100%" cellspacing="0">
+                    <table class="table table-striped table-bordered table-hover" id="tabelkelas" width="100%" cellspacing="0">
                         <thead>
                             <tr>
                                 <th>No.</th>
-                                <th>Barkode</th>
+                                <th>Barcode</th>
                                 <th>Name</th>
                                 <th>Satuan</th>
                                 <th>Kategori</th>
@@ -35,28 +35,30 @@
                             $no = 1;
                             foreach ($barang as $barang) {
                                 echo "<tr>
-                                <td>$no</td>
-                                <td>$barang->barkode</td>
-                                <td>$barang->name</td>
-                                <td>$barang->satuan</td>
-                                <td>$barang->kategori</td>
-                                <td>$barang->stok</td>
-                                <td>$barang->harga_beli</td>
-                                <td>$barang->harga_jual</td>
-                                <td>
-                                <div>
-                                    <a href=" . base_url('barang/getedit/' . $barang->id) . " class='btn btn-sm btn-sm btn-info'><i class='fas fa-edit'></i>Edit</a>
-                                    <a href=" . base_url('barang/delete/' . $barang->id) . " class='btn btn-sm btn-danger'
-                                    onclik='return confirm(\"Ingin menghapus data user ini\");'><i class='fas fa-trash'></i> hapus</a>
-                                    </div>
-                                </td>
-                            </tr>";
+                  <td>{$no}</td>
+                  <td>{$barang->barkode}</td>
+                  <td>{$barang->name}</td>
+                  <td>{$barang->satuan}</td>
+                  <td>{$barang->kategori}</td>
+                  <td>{$barang->stok}</td>
+                  <td>{$barang->harga_beli}</td>
+                  <td>{$barang->harga_jual}</td>
+                  <td>
+                    <div>
+                      <a href='" . base_url('barang/getedit/' . $barang->id) . "' class='btn btn-sm btn-info'><i class='fas fa-edit'></i> Edit</a>
+                      <a href='" . base_url('barang/delete/' . $barang->id) . "' class='btn btn-sm btn-danger' 
+                        onclick='return confirm(\"Ingin mengapus data kategori ini?\");'><i class='fas fa-trash'></i> Hapus</a>
+                    </div>
+                  </td>
+                </tr>";
                                 $no++;
                             } ?>
+
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
+        <div style="height: 100vh;"></div>
     </div>
 </main>
